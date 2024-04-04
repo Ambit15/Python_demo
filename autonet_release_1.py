d3aa4eaf57cf62f345a5e024e646b8e1994b721e
[Ambit15] <[1679516796@qq.com]>
2024-04-04 17:37:08 +0800

    log1

--- autonet_release_1.py
+++ autonet_release_1.py
@@ -3,9 +3,9 @@ import pyautogui as pg
 i = 1
 while i == 1:
     time.sleep(5)
-    pg.click(x = 1258,y = 437)
+    pg.click(x = 1286,y = 440)
     time.sleep(3)
-    pg.click(x = 1167,y = 512)
+    pg.click(x = 1194,y = 512)
     time.sleep(3)
-    pg.click(x = 1011,y = 786)
+    pg.click(x = 1027,y = 789)
     break
