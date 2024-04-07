46ceb2f7a4d2291c7bee5b6ebfa7f92ee1734552
Ambit15 <1679516796@qq.com>
2024-04-07 15:29:00 +0800

    execute2

--- python_code/Graduation/Gdal/execute.py
+++ python_code/Graduation/Gdal/execute.py
@@ -17,7 +17,7 @@ temp_night_folder = r"G:\graduation_database\Analysis_temp\night"
 city = []
 temp_shp_folder = "G:\\\\graduation_database\\\\Analysis_temp\\\\shp"
 temp_shp_dir = temp_shp_folder + "\\\\" + city_name + ".shp"
-base_shp_dir = r"G:\graduation_database\boundrye\base\陕西省.shp"
+base_shp_dir = r"G:\graduation_database\boundry\base\陕西省.shp"
 night_folder = "G:\\\\graduation_database\\\\Night"
 city.append(city_name)
 
