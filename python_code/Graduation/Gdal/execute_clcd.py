90a6eefecff010c0e729ee268701852eedb549f7
Ambit15 <1679516796@qq.com>
2024-04-13 17:31:31 +0800

    clcd_execute

--- python_code/Graduation/Gdal/execute_clcd.py
+++ python_code/Graduation/Gdal/execute_clcd.py
@@ -6,37 +6,43 @@ import gdal_summery_clcd as gsc
 from osgeo import ogr
 
 start_time = time.time()
-#可选参数 #####
-city_name = "铜川市"
-#"西安市" "咸阳市"  "宝鸡市"  "渭南市"  "商洛市"  "汉中市"  "安康市"  "榆林市"  "延安市"  "铜川市"
-clcd_folder = "G:\\\\graduation_database\\\\CLCD"
-#night_folder = "G:\\\\graduation_database\\\\Night"
-base_shp_dir = r"G:\graduation_database\boundry\base\陕西省.shp"
-#下一步时间
+city_list = ["西安市","咸阳市","宝鸡市","渭南市","商洛市","汉中市","安康市","榆林市","延安市","铜川市"]
 
-temp_folder = r"G:\graduation_database\Analysis_temp"
-temp_xls_folder = "G:\\\\graduation_database\\\\Analysis_temp\\\\xls"
-temp_clcd_folder = r"G:\graduation_database\Analysis_temp\temp_clcd"
+def range_clcd_city(name):
+    #可选参数 #####
+    city_name = name
+    #"西安市" "咸阳市"  "宝鸡市"  "渭南市"  "商洛市"  "汉中市"  "安康市"  "榆林市"  "延安市"  "铜川市"
+    clcd_folder = "G:\\\\graduation_database\\\\CLCD"
+    #night_folder = "G:\\\\graduation_database\\\\Night"
+    base_shp_dir = r"G:\graduation_database\boundry\base\陕西省.shp"
+    #下一步时间
 
-city = []
-temp_shp_folder = "G:\\\\graduation_database\\\\Analysis_temp\\\\shp"
-temp_shp_dir = temp_shp_folder + "\\\\" + city_name + ".shp"
+    temp_folder = r"G:\graduation_database\Analysis_temp"
+    xls_dir = "G:\\\\graduation_database\\\\calcuate\\\\model\\\\result_clcd" + "\\\\" + name + ".xls"
+    temp_clcd_folder = r"G:\graduation_database\Analysis_temp\temp_clcd"
 
-city.append(city_name)
+    city = []
+    temp_shp_folder = "G:\\\\graduation_database\\\\Analysis_temp\\\\shp"
+    temp_shp_dir = temp_shp_folder + "\\\\" + city_name + ".shp"
 
-#clear and rebuild folderee
-gs.clear_folder(temp_folder)
-gs.cerate_folder(temp_xls_folder)
-gs.cerate_folder(temp_shp_folder)
-gs.cerate_folder(temp_clcd_folder)
-#select boundry
-gs.SelectByAttribute(base_shp_dir,"name",ogr.OFTString,city,"WGS84",temp_shp_dir)
-#clip raster
-for filename in os.listdir(clcd_folder):
-    rester_dir = clcd_folder + "\\\\" + filename
-    gdal_clip.clip_rester(rester_dir,temp_shp_dir,temp_clcd_folder)
-#summery
+    city.append(city_name)
 
+    #clear and rebuild folderee
+    gs.clear_folder(temp_folder)
+    gs.cerate_folder(temp_shp_folder)
+    gs.cerate_folder(temp_clcd_folder)
+    #select boundry
+    gs.SelectByAttribute(base_shp_dir,"name",ogr.OFTString,city,"WGS84",temp_shp_dir)
+    #clip raster
+    for filename in os.listdir(clcd_folder):
+        rester_dir = clcd_folder + "\\\\" + filename
+        gdal_clip.clip_rester(rester_dir,temp_shp_dir,temp_clcd_folder)
+    #summery
+    gsc.summery_clcd(temp_clcd_folder,name,xls_dir)
+
+#range to clip and summary
+for i in city_list:
+    range_clcd_city(i)
 
 #caculate execute time
 end_time = time.time()
--- python_code/Graduation/Gdal/gdal_summery_clcd.py
+++ python_code/Graduation/Gdal/gdal_summery_clcd.py
@@ -14,17 +14,54 @@ def summery_clcd(folder,xlsname,save_dir):
     #create sheet
     sheet = workbook.add_sheet(xlsname,cell_overwrite_ok=True)
 
-    head = ["年度","0","1","2","3","4","5","6","7","8","9"]
+    head = ["年度","1","2","3","4","5","6","7","8","9"]
     years = []
+    start_x = 1
+    for filename in os.listdir(folder):
+        sum_list = []
+        year = gc.check_year(gc.keep_digits(filename))
+        years.append(year)
+        filedir = folder + "\\\\" + filename
+        with rasterio.open(filedir) as raster:
+            raster_data = raster.read()
 
-"""
+            band_index = 0  # 根据实际情况修改
+            #降维
+            band_data = raster_data[band_index].reshape(-1)
+            len_raster = len(band_data)
 
+            #band_data = np.fromiter(band_data,"uint8",count = -1)
+            ID_list = [1,2,3,4,5,6,7,8,9]
+            #数组计数
+            #counts = np.bincount(band_data)
+            for i in ID_list:
+                count = round(np.sum(band_data == i) / len_raster,6)
+                sum_list.append(count)
+            print(sum_list)
+            start_y = 1
+            for i in sum_list:
+                sheet.write(start_x,start_y,float(i))
+                start_y = start_y + 1
+            start_x = start_x + 1
+            #print(sum_list)
+    
+    for i in head:
+        sheet.write(0,head.index(i),i)
+
+    for i in years:
+        sheet.write(years.index(i)+1,0,i)
+    
+    workbook.save(save_dir)
+
+#函数测试成功
+#summery_clcd(r"G:\graduation_database\Analysis_temp\temp_clcd","test",r"G:\graduation_database\Analysis_temp\xls\test.xls")
 """
 with rasterio.open(r"G:\graduation_database\Analysis_temp\temp_clcd\011990_clip.tif") as raster:
     raster_data = raster.read()
 
     band_index = 0  # 根据实际情况修改
     band_data = raster_data[band_index].reshape(-1)
+    len_raster = len(band_data)
 
     #降维
     #band_data = np.fromiter(band_data,"uint8",count = -1)
@@ -35,3 +72,4 @@ with rasterio.open(r"G:\graduation_database\Analysis_temp\temp_clcd\011990_clip.
     for i in ID_list:
         count = np.sum(band_data == i)
     print(sum_list)
+"""
