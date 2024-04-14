c70d28828a2d8301ada1f060fbc1ad9926c7a7d2
Ambit15 <1679516796@qq.com>
2024-04-14 14:46:33 +0800

    xlwt

--- python_code/Graduation/test.py
+++ python_code/Graduation/test.py
@@ -1,6 +1,10 @@
 import re
+import os
 import numpy as np
+import xlrd
+import xlwt
 
+"""
 def keep_digits(string):
     return re.sub("\D", "", string)
 
@@ -29,3 +33,38 @@ for i in range(5):
 xls_dir = r"G:\graduation_database\calcuate\model\result_clcd\安康市.xls"
 city_name = xls_dir[-7:-4]
 print(city_name)
+
+sum_folder = r"G:\graduation_database\calcuate\model\city_sum"
+mean_folder = r"G:\graduation_database\calcuate\model\city_mean"
+return_folder = r"G:\graduation_database\calcuate\model\clcd_result_img"
+
+for filename in os.listdir(sum_folder):
+    xls_sum_dir = sum_folder + "\\\\" + filename
+    xls_mean_dir = mean_folder + "\\\\" + filename
+    xls_dir =  return_folder + "\\\\" +filename
+
+    wb_sum = xlrd.open_workbook(xls_sum_dir)
+    sheet_sum = wb_sum.sheet_by_index(0)
+    list_sum = sheet_sum.col_values(1)[17:]
+    #print(list_sum)
+
+    wb_mean = xlrd.open_workbook(xls_mean_dir)
+    sheet_mean = wb_mean.sheet_by_index(0)
+    list_mean = sheet_mean.col_values(2)[17:-1]
+    #print(list_mean)
+    
+    #print(filename[0:3])
+    workbook = xlwt.Workbook(encoding="utf-8")
+    #create sheet
+    sheet = workbook.add_sheet(filename[0:3],cell_overwrite_ok=True)
+    
+    for i in list_mean:
+        sheet.write(list_mean.index(i),0,i)
+
+    for i in list_sum:
+        sheet.write(list_sum.index(i),1,i)
+
+    workbook.save(xls_dir)
+
+"""
+
